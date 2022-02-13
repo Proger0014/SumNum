84a25aa775083ebd6732e4b71d92b34e615232ac
Proger0014 <iakov56@yandex.ru>
2022-02-13 16:35:49 +0600

    - Добавил несколько других реалиацию метода Sum

--- SumNum.BusinessLogic/SumClass.cs
+++ SumNum.BusinessLogic/SumClass.cs
@@ -5,5 +5,7 @@ namespace SumNum.BusinessLogic
     public class SumClass
     {
         public int Sum(int a, int b) => a + b;
+        public int Sum2(int a, int b) => a + b + 23;
+        public int Sum3(int a, int b) => a + b + 46;
     }
 }
--- SumNum/SumTwoNums3.cs
+++ SumNum/SumTwoNums3.cs
@@ -1,19 +1,19 @@
-﻿using System;
+﻿using SumNum.BusinessLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SumNum.BusinessLogic;
 
 namespace SumNum.API
 {
-    public class SumTwoNums2 : ISumTwoNums
+    public class SumTwoNums3 : ISumTwoNums
     {
         public int Sum(int a, int b)
         {
             SumClass sumClass = new SumClass();
 
-            return sumClass.Sum2(a, b);
+            return sumClass.Sum3(a, b);
         }
     }
 }
